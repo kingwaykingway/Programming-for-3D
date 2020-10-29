009c608093886a0769c7f5268aa93a1bcd176be7
KingwayKingway <1040123009@qq.com>
2020-10-29 14:35:40 +0000

    week 5 session 1

---
+++ week 4 session 2a/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    [SerializeField] private string otherTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == otherTag )
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
