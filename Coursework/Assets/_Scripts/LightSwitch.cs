665934f5fb46c1337b8ecac42fe832aed4080927
KingwayKingway <1040123009@qq.com>
2021-01-13 17:02:52 +0000

    2021/1/13 #1
    Last commit before exporting.

--- Coursework/Assets/_Scripts/LightSwitch.cs
+++ Coursework/Assets/_Scripts/LightSwitch.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
-
 
 public class LightSwitch : MonoBehaviour
 {
     [SerializeField] private List<GameObject> switchableObjects;
     
     private bool _isSwitchedOn;
-
-    private void Start()
+    
+    void Start()
     {
         foreach (var o in switchableObjects)
         {
@@ -26,39 +24,10 @@ public class LightSwitch : MonoBehaviour
     public void Toggle()
     {
         _isSwitchedOn = !_isSwitchedOn;
-        
+
         foreach (var o in switchableObjects)
         {
             o.SetActive(_isSwitchedOn);
         }
     }
 }
-
-/*public class LightSwitch : MonoBehaviour
-{
-    [SerializeField] private GameObject lightObject;
-    [SerializeField] private GameObject audioObject;
-    
-    private bool _isSwitchedOn;
-    
-    void Start()
-    {
-        _isSwitchedOn = lightObject != null && lightObject.active 
-                        || audioObject != null && audioObject.active;
-    }
-
-    public void Toggle()
-    {
-        _isSwitchedOn = !_isSwitchedOn;
- 
-        if (lightObject != null)
-        {
-            lightObject.SetActive(_isSwitchedOn);
-        }
-
-        if (audioObject != null)
-        {
-            audioObject.SetActive(_isSwitchedOn);
-        }
-    }
-}*/
